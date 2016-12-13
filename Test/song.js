3accdaf618d38c839aa9000afe1a1145f03896d7
Maria Kilsved <Maria Kilsved>
2016-12-13 19:50:37 +0100

    Cleanup

--- Test/song.js
+++
@@ -1,7 +0,0 @@
-function Song(title, artist){
-    //Låten ska ha två parametrar: title, artist
-    //Förutom det ska låten även ha en bool: isPlaying
-    this.title = title;
-    this.artist = artist;
-    this.isPlaying = false;
-}
--- Test/try.js
+++
@@ -1,2 +0,0 @@
-var a = isNaN("10");
-console.log(a);
