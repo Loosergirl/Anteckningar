4387f9612b4251c30a494033f084ba3225e8c11e
Maria Kilsved <Maria Kilsved>
2018-01-23 12:40:20 +0100

    Folder move

--- JavaScript3/react save/src/components/Login.js
+++ JavaScript3/react save/src/components/Login.js
@@ -1,38 +1,102 @@
 import React, { Component } from 'react';
+import Confirmation from './Confirmation.js';
 
-class Login extends Component{
-	render(){
-        return(
-          <div>
-          <form onSubmit={this.props.onSubmit}>
-            <div className="form-group">
-			  <label htmlFor="email">Email</label>
-              <input
-                type="email"
-                name="email" id="email"
-                className="form-control m-3" placeholder="name@example.com"
-                value={this.props.email}
-                onChange={this.props.onChange}
-              />
-		
-			  <label htmlFor="password">Password</label>
-              <input
-                type="password"
-                name="password"
-                className="form-control m-3" id="password" placeholder="Password"
-                value={this.props.password}
-                onChange={this.props.onChange}
-              />
-		
+class Login extends Component {
+    state = {
+        email: "",
+        password: "",
+        user: "",
+        message: ""
+    };
+
+    componentDidMount() {
+        firebase.auth()
+            .onAuthStateChanged((user) => {
+                if (user) {
+                    this.setState({
+                        user: user
+                    });
+                } else {
+                    this.setState({
+                        user: ''
+                    })
+                }
+            })
+    }
+
+    onSubmit = e => {
+        e.preventDefault();
+        firebase.auth()
+            .createUserWithEmailAndPassword(this.state.email, this.state.password)
+            .then(user => this.setState({ message: "Created new user"}))
+            .catch(error => this.setState({ message: error}))
+    };
+
+    signIn = () => {
+        firebase.auth()
+            .signInWithEmailAndPassword(this.state.email, this.state.password)
+            .catch(error => this.setState({ message: error}));
+    }
+
+    signOut = () => {
+        firebase.auth().signOut();
+    }
+
+    onChange = e => this.setState({
+        [e.target.name]: e.target.value
+    });
+
+    render() {
+        return ( 
+            <div>
+            <form className="less-padding" onSubmit = { this.onSubmit }>
+            <div className = "form-group">
+                
+            <label htmlFor = "email" > Email < /label> 
+            <input type = "email"
+              name = "email"
+              id = "email"
+              className = "form-control"
+              placeholder = "name@example.com"
+              value = {
+                  this.state.email
+              }
+              onChange = {
+                  this.onChange
+              }
+            />
+
+            <label htmlFor = "password" > Password < /label> 
+            <input type = "password"
+              name = "password"
+              className = "form-control"
+              id = "password"
+              placeholder = "Password"
+            />
+
+            </div> 
+            
+            <input type = "submit"
+              value = "Register"
+              className = "btn btn-success" / >
+            
+            </form>
+
+            {
+                !this.state.user && < button className = "btn btn-primary"
+                onClick = {
+                    this.signIn
+                } > Login </button>
+            } {
+                this.state.user && < button className = "btn btn-primary"
+                onClick = {
+                    this.signOut
+                } > Sign Out < /button> 
+            } 
+            
+            <Confirmation text={ this.state.message } />
+            
             </div>
-            <input type="submit" value="Register" className="btn btn-success m-3" />
-          </form>
-		
-          { !this.props.user && <button className="btn btn-primary m-3" onClick={this.props.signIn}> Login </button>
-		  }
-          { this.props.user && <button className="btn btn-primary m-3" onClick={this.props.signOut}> Sign out </button> 
-		  }
-        </div>
         );
     }
 }
